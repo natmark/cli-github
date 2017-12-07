225a5ff15531b97e659d1c3f85c403a37743ee2b
natmark <natmark0918@gmail.com>
2017-12-07 22:34:23 +0900

    :sparkles: Add create feature

--- lib/github/cli.rb
+++ lib/github/cli.rb
@@ -1,5 +1,8 @@
 require 'thor'
 require 'json'
+require 'net/http'
+require 'uri'
+require 'highline'
 
 module Github
     class ConfigManager
@@ -44,7 +47,49 @@ module Github
         desc 'create github repo', 'create repository on GitHub'
         def create(repo_name)
             config = ConfigManager.load()
-            exec("curl -H 'Authorization: token #{config['token']}' https://api.github.com/user/repos -d '{\"name\":\"#{repo_name}\"}'")
+
+            uri = URI.parse("https://api.github.com/user/repos")
+            https = Net::HTTP.new(uri.host, uri.port)
+
+            https.use_ssl = true
+            req = Net::HTTP::Post.new(uri.request_uri)
+
+            req["Content-Type"] = "application/json"
+            req["Authorization"] = "token #{config['token']}"
+
+
+            cli = HighLine.new
+            description = cli.ask("A short description of the repository: ")
+
+            private = cli.agree("Create a private repository? [Y/n]")
+            auto_init = cli.agree("Create an initial commit with empty README? [Y/n]")
+
+            gitignore_template = cli.ask("Use the name of the template without the extension. (https://github.com/github/gitignore)") {|q| q.default = 'C++' }
+            license_template = cli.ask("Choose an open source license template that best suits your needs. (https://help.github.com/articles/licensing-a-repository/#searching-github-by-license-type)") {|q| q.default = 'mit' }
+
+            payload = {
+                "name" => repo_name,
+                "description" => description,
+                "private" => private,
+                "auto_init" => auto_init,
+                "gitignore_template" => gitignore_template,
+                "license_template" => license_template
+            }.to_json
+
+            req.body = payload
+            res = https.request(req)
+            json = JSON.parse(res.body)
+
+            if res.code.to_i.between?(200,300)
+                puts "==========SUCCESS=========="
+                puts "repo_name: #{json["full_name"]}"
+                puts "url: #{json["url"]}"
+                puts "git_url: #{json["git_url"]}"
+                puts "isPrivate: #{json["private"]}"
+                puts "description: #{json["description"]}"  if json["description"] != nil
+            else
+                puts "ERROR: " + json["errors"][0]["message"]
+            end
         end
 
 
